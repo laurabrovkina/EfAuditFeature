984527004c6dcc42489345e829f9f5123bad6beb
Larisa <laurabrovkina@gmail.com>
2024-10-02 12:11:22 +1300

    Use swagger

--- EfAuditFeature/Program.cs
+++ EfAuditFeature/Program.cs
@@ -6,7 +6,8 @@ using MassTransit;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
 
 builder.Services.AddKeyedScoped<List<AuditEntity>>("Audit", (_,_) => new());
@@ -29,7 +30,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapControllers();
