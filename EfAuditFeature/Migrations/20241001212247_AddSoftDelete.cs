9b9d458f9c699184350a2187ce6f3fd975c7f504
Larisa <laurabrovkina@gmail.com>
2024-10-02 12:14:27 +1300

    Run migration

---
+++ EfAuditFeature/Migrations/20241001212247_AddSoftDelete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GitHubCopilotExamles.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAtUtc",
+                table: "People",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "People",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAtUtc",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "People");
+        }
+    }
+}
