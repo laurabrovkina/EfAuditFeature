6857b90c56cee7e80376dbad7351f82146d19a87
Larisa <laurabrovkina@gmail.com>
2024-10-02 12:12:38 +1300

    Add ISoftDeletable to Person Model

--- EfAuditFeature/Models/Person.cs
+++ EfAuditFeature/Models/Person.cs
@@ -1,11 +1,22 @@
-﻿namespace EfAuditFeathre.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Person
+namespace EfAuditFeathre.Models;
+
+public class Person : ISoftDeletable
 {
+    [Key]
     public Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAtUtc { get; set; }
+}
+
+public interface ISoftDeletable
+{
+    public bool IsDeleted  { get; set; }
+    public DateTime? DeletedAtUtc { get; set; }
 }
