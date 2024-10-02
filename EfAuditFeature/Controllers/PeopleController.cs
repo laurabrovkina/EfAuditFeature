933264decc3ea7a8335f83d88ef1e3e41d58fa25
Larisa <laurabrovkina@gmail.com>
2024-10-02 17:11:58 +1300

    Update controller

--- EfAuditFeature/Controllers/PeopleController.cs
+++ EfAuditFeature/Controllers/PeopleController.cs
@@ -2,66 +2,64 @@ using Microsoft.AspNetCore.Mvc;
 using EfAuditFeathre.Services;
 using EfAuditFeathre.Models;
 using EfAuditFeathre.Publisher;
+using static MassTransit.ValidationResultExtensions;
 
-namespace EfAuditFeathre.Controllers
+namespace EfAuditFeathre.Controllers;
+
+[Route("people")]
+[ApiController]
+public class PeopleController : ControllerBase
 {
-    public class PeopleController : ControllerBase
+    private readonly IPersonService _personService;
+    private readonly IPublishEvents _publishEvents;
+
+    public PeopleController(IPersonService personService,
+        IPublishEvents publishEvents)
     {
-        private readonly IPersonService _personService;
-        private readonly IPublishEvents _publishEvents;
+        _personService = personService;
+        _publishEvents = publishEvents;
+    }
 
-        public PeopleController(IPersonService personService,
-            IPublishEvents publishEvents)
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var people = await _personService.GetAll();
+        return Ok(people);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> Get(Guid id)
+    {
+        var person = await _personService.GetById(id);
+        return person is not null ? Ok(person) : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Person person)
+    {
+        await _personService.Create(person);
+        
+        await _publishEvents.PublishMessageAsync(person);
+
+        return CreatedAtAction(nameof(Get), new { id = person.Id }, person);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] Person person)
+    {
+        var existingPerson = _personService.GetById(id);
+        if (existingPerson == null)
         {
-            _personService = personService;
-            _publishEvents = publishEvents;
+            return NotFound();
         }
+        await _personService.Update(person);
+        return Ok(person);
+    }
 
-        [HttpGet("people")]
-        public IActionResult GetAll()
-        {
-            var people = _personService.GetAll();
-            return Ok(people);
-        }
-
-        [HttpGet("people/{id}")]
-        public IActionResult Get(Guid id)
-        {
-            var person = _personService.GetById(id);
-            if (person == null)
-            {
-                return NotFound();
-            }
-            return Ok(person);
-        }
-
-        [HttpPost("people")]
-        public IActionResult Create([FromBody] Person person)
-        {
-            _personService.Add(person);
-
-            _publishEvents.PublishMessageAsync(person);
-
-            return Ok();
-        }
-
-        [HttpPut("people/{id}")]
-        public IActionResult Update(Guid id, [FromBody] Person person)
-        {
-            var existingPerson = _personService.GetById(id);
-            if (existingPerson == null)
-            {
-                return NotFound();
-            }
-            _personService.Update(person);
-            return Ok();
-        }
-
-        [HttpDelete("people/{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var deleted = await _personService.DeleteById(id);
-            return deleted ? Ok() : NotFound();
-        }
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var deleted = await _personService.DeleteById(id);
+        return deleted ? Ok() : NotFound();
     }
 }
